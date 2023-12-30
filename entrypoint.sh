6b6b6f25605c838b01bb7a13d2bfb4532b4eef7e
Josh Levin <morpheus636@morpheus636.com>
2023-12-30 15:43:09 -0500

    Generate secrets if wp-secrets.php is a placeholder

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,11 @@ if [ ! "$(ls -A "/var/www/wp-content" 2>/dev/null)" ]; then
     # Copy wp-content from Wordpress src to volume
     cp -r /usr/src/wordpress/wp-content /var/www/
     chown -R nobody.nobody /var/www
+fi
 
+# Check if wp-secrets.php is a placeholder file
+if grep -q "This is a placeholder file." /usr/src/wordpress/wp-secrets.php; then
+    echo "Generating wp-secrets.php"
     # Generate secrets
     curl -f https://api.wordpress.org/secret-key/1.1/salt/ >> /usr/src/wordpress/wp-secrets.php
 fi
--- wp-secrets.php
+++ wp-secrets.php
@@ -1,2 +1,5 @@
 <?php
-/** Generated file based on https://api.wordpress.org/secret-key/1.1/salt/ */
+/** This is a placeholder file. It will be replaced with a generated file
+ * based on https://api.wordpress.org/secret-key/1.1/salt/ when the container
+ * is first launched.
+ * /
