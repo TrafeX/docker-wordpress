e387452d05fd67e2de8930f97cf69257d41aa79e
Tim de Pater <git@trafex.nl>
2025-01-04 17:04:06 +0100

    Set smoketest Alpine Linux version to 3.21 and fix deprecated chown syntax

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 apk --no-cache add curl
-while ! curl -fs wordpress > /dev/null; do echo -n '.'; sleep 1; done;
-curl --silent --fail http://wordpress/wp-admin/install.php | grep 'wp-core-ui' > /dev/null
+for i in $(seq 1 10); do curl -fs wordpress > /dev/null && break || { echo -n '.'; sleep 1; }; done;
+curl --silent --fail http://wordpress/wp-admin/install.php | grep 'wp-core-ui'
